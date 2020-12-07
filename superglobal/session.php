df43f08e2fd023ee3acdb77fa87065aa5798e820
samiul-sheikh <75366327+samiul-sheikh@users.noreply.github.com>
2020-12-08 04:48:12 +0600

    Add files via upload

---
+++ superglobal/session.php
@@ -0,0 +1,66 @@
+<?php
+session_start();
+
+?>
+
+<!docttype html>
+<html>
+    <head>
+        <title> PHP Syntax </title>
+
+        <style>
+        	.phpcoding{width:900px; margin:0 auto; background:grey; min-height: 400px;}
+
+        	.headerportion, .footerportion{background:#444; color:white; text-align: center; padding: 20px;}
+
+        	.headerportion h2, .footerportion h2{margin:0;}
+
+        	.maincontent{min-height: 400px; padding: 20px;}
+
+             p{margin: 0}
+
+        </style>
+
+    </head>
+
+
+    <body>
+
+    	<div class ="phpcoding">
+
+
+    		<section class="headerportion">
+    			<h2> <?php echo "PHP Fundamental" ?> </h2>
+    		</section>
+
+
+    		<section class="maincontent">
+
+		    	<?php
+
+		    		$_SESSION['Usename'] = " Samiul";
+                    $_SESSION['Password'] = " 12345";
+
+
+                    echo "Usename is".$_SESSION['Usename']."<br/>";
+                    // session_unset();
+                    echo "Password is".$_SESSION['Password'];
+                    session_destroy();
+
+    			?>
+
+    		</section>
+
+
+        	<section class="footerportion">
+                <p> &copy; <?php echo date("Y") ?> Samiul Sheikh </p>
+        		<h2> <?php echo "Presented By - Samiul Sheikh" ?> </h2>
+        	</section>
+
+
+    	</div>
+
+
+    </body>
+
+</html>
