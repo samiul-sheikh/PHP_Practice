3e592c9f04a261caa6b5d9e3bb6fc7e0fde11948
samiul-sheikh <75366327+samiul-sheikh@users.noreply.github.com>
2020-12-09 23:06:57 +0600

    Add files via upload

---
+++ error_reporting.php
@@ -0,0 +1,67 @@
+<!docttype html>
+<html>
+    <head>
+        <title> PHP Syntax </title>
+
+        <style>
+        	.phpcoding{width:900px; margin:0 auto; background:grey; min-height: 400px;}
+
+        	.headerportion, .footerportion{background:#444; color:white; text-align: center; padding: 20px;}
+
+        	.headerportion h2, .footerportion h2{margin:0;}
+
+        	.maincontent{min-height: 400px; padding: 20px;}
+
+             p{margin: 0}
+
+        </style>
+
+    </head>
+
+
+    <body>
+
+    	<div class ="phpcoding">
+
+
+    		<section class="headerportion">
+    			<h2> <?php echo "PHP Fundamental" ?> </h2>
+    		</section>
+
+
+    		<section class="maincontent">
+
+		    	<?php
+
+                    // phpinfo();
+
+                    error_reporting(E_ERROR | E_WARNING | E_PARSE | E_NOTICE);
+
+                    $price = 45;
+
+                    if($price == 45){
+                        echo "The price is $price";
+                    }
+                    else{
+                        "price is not $price";
+                    }
+
+		    		
+
+    			?>
+
+    		</section>
+
+
+        	<section class="footerportion">
+                <p> &copy; <?php echo date("Y") ?> Samiul Sheikh </p>
+        		<h2> <?php echo "Presented By - Samiul Sheikh" ?> </h2>
+        	</section>
+
+
+    	</div>
+
+
+    </body>
+
+</html>
