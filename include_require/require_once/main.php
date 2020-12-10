05e8ce8fd1b52e258d70f8469dda9a00fc59d1f8
samiul-sheikh <75366327+samiul-sheikh@users.noreply.github.com>
2020-12-11 02:39:04 +0600

    Add files via upload

---
+++ include_require/require_once/main.php
@@ -0,0 +1,23 @@
+<?php include "header.php" ?>
+
+
+    		<section class="headerportion">
+    			<h2> <?php echo "PHP Fundamental" ?> </h2>
+    		</section>
+
+
+    		<section class="maincontent">
+                
+
+		    	<?php
+
+		    		require "new.php";
+                    require_once "new.php";
+
+
+    			?>
+
+    		</section>
+
+
+        	<?php require "footer.php" ?>
