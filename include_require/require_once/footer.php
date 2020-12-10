fb1909911c761c11e1255fe9bca0dd6effad0f55
samiul-sheikh <75366327+samiul-sheikh@users.noreply.github.com>
2020-12-11 02:40:53 +0600

    Add files via upload

---
+++ include_require/require_once/footer.php
@@ -0,0 +1,12 @@
+<section class="footerportion">
+                <p> &copy; <?php echo date("Y") ?> Samiul Sheikh </p>
+        		<h2> <?php echo "Presented By - Samiul Sheikh" ?> </h2>
+        	</section>
+
+
+    	</div>
+
+
+    </body>
+
+</html>
