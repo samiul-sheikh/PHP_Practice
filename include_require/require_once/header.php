efdc025b395bed675cb28b9bff5c55f71cfb76e6
samiul-sheikh <75366327+samiul-sheikh@users.noreply.github.com>
2020-12-11 02:40:21 +0600

    Add files via upload

---
+++ include_require/require_once/header.php
@@ -0,0 +1,24 @@
+<!docttype html>
+<html>
+    <head>
+        <title> PHP Syntax </title>
+
+        <style>
+            .phpcoding{width:900px; margin:0 auto; background:grey; min-height: 400px;}
+
+            .headerportion, .footerportion{background:#444; color:white; text-align: center; padding: 20px;}
+
+            .headerportion h2, .footerportion h2{margin:0;}
+
+            .maincontent{min-height: 400px; padding: 20px;}
+
+             p{margin: 0}
+
+        </style>
+
+    </head>
+
+
+    <body>
+
+        <div class ="phpcoding">
